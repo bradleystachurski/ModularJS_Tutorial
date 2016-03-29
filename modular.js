4bcc54edc084db82e1fd643c2ce73949cf785ace
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-03-29 16:07:18 -0500

    Working module with object literal

--- modular.js
+++ modular.js
@@ -1,18 +1,44 @@
 /**
  * Created by Bradley on 3/28/16.
  */
-function() {
+(function() {
     var people = {
-        people: [],
-        template: $('#people-template').html(),
+        people: ['Brad', 'Brian'],
         init: function() {
             this.cacheDom();
-        }
+            this.bindEvents();
+            this.render();
+        },
         cacheDom: function() {
             this.$el = $('#peopleModule');
             this.$button = this.$el.find('button');
             this.$input = this.$el.find('input');
             this.$ul = this.$el.find('ul');
+            this.template = this.$el.find('#people-template').html();
+        },
+        bindEvents: function() {
+            this.$button.on('click', this.addPerson.bind(this));
+            this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
+        },
+        render: function() {
+            var data = {
+                people: this.people
+            };
+            this.$ul.html(Mustache.render(this.template, data));
+        },
+        addPerson: function() {
+            this.people.push(this.$input.val());
+            this.render();
+            this.$input.val('');
+        },
+        deletePerson: function(event) {
+            var $remove = $(event.target).closest('li');
+            var i = this.$ul.find('li').index($remove);
+
+            this.people.splice(i, 1);
+            this.render();
         }
     };
-}();
+
+    people.init();
+})()
