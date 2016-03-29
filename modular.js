e96fc1493cabfa6357163a5377c6218c47c0540e
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-03-28 22:24:20 -0500

    Time for bed

--- modular.js
+++ modular.js
@@ -1,3 +1,18 @@
 /**
  * Created by Bradley on 3/28/16.
  */
+function() {
+    var people = {
+        people: [],
+        template: $('#people-template').html(),
+        init: function() {
+            this.cacheDom();
+        }
+        cacheDom: function() {
+            this.$el = $('#peopleModule');
+            this.$button = this.$el.find('button');
+            this.$input = this.$el.find('input');
+            this.$ul = this.$el.find('ul');
+        }
+    };
+}();
